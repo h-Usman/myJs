0b7a4a7212117513c2e57529981e2b8b29c50ece
h-usman <hafizusman7272@gmail.com>
2025-02-01 19:32:47 +0500

    get starting and basics flder are done

---
+++ js-basic/index.js
@@ -0,0 +1,80 @@
+// console.log('Hello World');
+
+// let name ;
+
+// let name = 'hafiz';
+// console.log(name);
+
+// Rules for variable declarations
+
+// 1 Cannot be reserve keyword
+// 2 Should be meaningfell 
+// 3 Cannot start with a number (1name)
+// 4 Camnnot contain space or hypen (-)
+// 5 Are case-sensitive
+// let firstName = 'Muhammad', lastName = 'Usman'
+// console.log(firstName);
+// console.log(lastName);
+
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// let name = 'Usman';  // string
+// let age = 30; // number
+// let isApproved = false; // boolean
+// let firstName = undefined; // undefined
+// let selectedColor = null; // object but why.?
+
+let person = {
+name: 'Usman',
+age:30
+}
+
+// dot notation
+// person.name= 'Hafiz'
+
+// bracket notations
+// person['name'] = 'muhammad'
+
+
+// let selection = 'name';
+// person[selection] = 'Hafiz Usman'
+// console.log(person.name);
+
+// console.log(person['name']);
+// console.log(person.name);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2]= 'green';
+// selectedColors[3]= 1;
+// console.log(selectedColors);
+// console.log(selectedColors.length);
+// console.log(typeof selectedColors);
+// console.log(typeof selectedColors[0]);
+// console.log(typeof selectedColors[3]);
+
+
+// Performing a task
+
+// function greet(firstName, lastName){
+//     console.log('Hello ' + firstName + ' ' + lastName);
+// }
+
+// greet('Usman', 'sab');
+// greet('Hafiz');
+
+// performing a value
+// function squre(number) {
+//    return number * number
+// }
+
+// console.log(squre(2));
+
+// let number = squre(2);
+// console.log(number);
